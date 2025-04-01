e9011dc2573a9dd02f682ef3e043ea9e57b4973e
annruiblaz <anna.ruiz.dev@gmail.com>
2025-04-01 18:12:23 +0200

    flappy class added + jump + score (temp)

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 // Variables globales para gestionar el juego
 let pajaro, fondo, tuberiaInferior;
-let posPajaroY = 300;
+let posPajaroY = 200;
 let velocidad = 0;
-let gravedad = 0.6 ;
+let gravedad = 0.6;
 let fuerzaSalto = -20;
 let tuberias = [];
 let espacioTuberias = 100; // Espacio constante entre tuberías
-let velocidadTuberias = 1.5  ;
+let velocidadTuberias = 1.5;
 let cantidadTuberias = 4; // Número de tuberías reutilizables
 let anchoTuberia = 50; // Ancho de la imagen de la tubería
 let canJump = true;
+let neat;
+let pruebaFlappy;
+let bestScore = 0;
+let pipeHeight;
 
 // Precarga las imágenes del juego
 function preload() {
@@ -22,15 +26,18 @@ function preload() {
 function setup() {
 	createCanvas(500, 600);
 	// Inicializa las tuberías en distintas posiciones
-    tuberias.push(new Tuberia(130));
-    tuberias.push(new Tuberia(130 + anchoTuberia + espacioTuberias));
-    tuberias.push(new Tuberia(130+ (anchoTuberia + espacioTuberias) * 2));
-    tuberias.push(new Tuberia(130+ (anchoTuberia + espacioTuberias) * 3));
-
+	tuberias.push(new Tuberia(150));
+	tuberias.push(new Tuberia(150 + anchoTuberia + espacioTuberias));
+	tuberias.push(new Tuberia(150 + (anchoTuberia + espacioTuberias) * 2));
+	tuberias.push(new Tuberia(150 + (anchoTuberia + espacioTuberias) * 3));
 
 /* 	for (let i = 0; i < cantidadTuberias; i++) {
 		tuberias.push(new Tuberia(i * (espacioTuberias + anchoTuberia) + width));
 	} */
+
+	pipeHeight = (tuberias[0].posY -32);
+
+	pruebaFlappy = new Flappy();
 }
 
 // Bucle principal del juego
@@ -43,109 +50,153 @@ function draw() {
 		tuberias[i].mostrar();
 		// Reubica las tuberías cuando salen de la pantalla
 		if (tuberias[i].fueraPantalla()) {
-			let ultimaX = Math.max(...tuberias.map(t => t.x)); // Encuentra la tubería más a la derecha
+			let ultimaX = Math.max(...tuberias.map(t => t.xStart+ anchoTuberia));
 			tuberias[i].reiniciar(ultimaX + espacioTuberias + anchoTuberia);
 		}
 
-        //TODO: revisar para dar + margen
+		//TODO: revisar para dar + margen
 		//comprueba si la tuberia se encuentra en el espacio del flappy (100-140px del canvas)
-        if(tuberias[i].x >= 80  && tuberias[i].x <= 130 ) {
-            //console.log('La pipe está en el eje x a la altura del flappy');
+		if (tuberias[i].xStart+50 >= 95 && tuberias[i].xStart <= 145) {
+			//console.log('La pipe está en el eje x a la altura del flappy');
 
-/*             console.log('PosY de la pipe', tuberias[i].posY);
-            console.log('PosY de flappy', posPajaroY); */
+			//para comprobar q x estamos detectando!
+			stroke('red');
+			line(parseInt(tuberias[i].xStart+50).toFixed(), 0, parseInt(tuberias[i].xStart+50).toFixed(), 600);// x derecha
+			line(parseInt(tuberias[i].xStart).toFixed(), 0, parseInt(tuberias[i].xStart).toFixed(), 600);// x derecha
+
 			//si el flappy esta por encima de la pipe (en el eje y)
-            if(posPajaroY < tuberias[i].posY) {
-                //console.log('Flappy sobre pipe');
-				if(posPajaroY >=   tuberias[i].posY - 32 ) {
-					posPajaroY = tuberias[i].posY - 32;
-					velocidad = 0;
+			if (pruebaFlappy.y < tuberias[i].posY) {
+				//console.log('Flappy sobre pipe');
+				if (pruebaFlappy.y >= pipeHeight) {
+					pruebaFlappy.y = pipeHeight;
+					pruebaFlappy.velocidad = 0;
 					canJump = true;
 				} else {
 					canJump = false;
 				}
-            }
-        } else {//si la tuberia no se encuentra dnd el flappy (es decir está entre el hueco d las tuberias)
+			}
+		} else { //si la tuberia no se encuentra dnd el flappy (es decir está entre el hueco d las tuberias)
 			//y si el flappy se encuentra x abajo d la altura de la tuberia (está en el hueco)
-            if(posPajaroY > tuberias[i].posY) {
+			if (pruebaFlappy.y > tuberias[i].posY) {
 				canJump = false;
-            }
-        }
-
-		//pinta un circulo dnd en el punto en el q coinciden el flappy y la pipe
-		if(canJump) {
-			fill(255, 0, 0);
-			ellipse(120, tuberias[i].posY - 15, 10, 10);
+				pruebaFlappy.estaVivo = false;
+				pruebaFlappy.puntuacion = 0;
+			}
 		}
-	}   
 
-	// pinta el flappy img, x, y, width, height
-	image(pajaro, 100, posPajaroY, 40, 30);
-	
-	// Aplica la gravedad al pájaro
-	velocidad += gravedad;
-	posPajaroY += velocidad;
+		if(pruebaFlappy.estaVivo) {
+			pruebaFlappy.puntuacion ++;
+		}
 
-	// Evita que el pájaro caiga fuera de la pantalla
-	if (posPajaroY > height - 30) {
-		posPajaroY = height - 30;
-		velocidad = 0;
+		//pinta lineas para visualizar los puntos q utilizo d ref
+		stroke('#FFF');
+		line(95, 0, 95, 600); // x izq
+		//line(145, 0, 145, 600);// x derecha
+		line(95, pipeHeight, 145, pipeHeight); //punto d "altura d la pipe" q es dnd se posiciona el flappy
+
+	}
+
+	// pinta la img del flappy
+	pruebaFlappy.show();
+	// Aplica la gravedad al flappy
+	pruebaFlappy.update();
+
+	if(pruebaFlappy.puntuacion > bestScore) {
+		bestScore = pruebaFlappy.puntuacion;
+	}
+
+	// Evita que el flappy caiga fuera de la pantalla
+	if (pruebaFlappy.y > height - 30) {
+		pruebaFlappy.y = height - 30;
+		pruebaFlappy.velocidad = 0;
+		pruebaFlappy.estaVivo = false;
 	}
 
 	mostrarDatos();
 }
 
 function mostrarDatos() {
-    fill('#000');
-    textSize(16);
-    text(`posPajaroY: ${posPajaroY.toFixed(1)}`, 20, 20);
-    text(`velocidad: ${velocidad.toFixed(1)}`, 20, 40);
-    text(`canJump: ${canJump}`, 20, 60);
+	fill('#000');
+	textSize(16);
+	text(`Flappy y: ${posPajaroY.toFixed(1)}`, 10, 20);
+	text(`Velocidad: ${velocidad.toFixed(1)}`, 10, 40);
+	text(`canJump: ${canJump}`, 10, 60);
+	text(`Puntuación Flappy: ${bestScore}`, 10, 80);
+	text(`Record Total: ${bestScore}`, 10, 100);
+
 }
 
 
 // Función que detecta cuando se presiona una tecla
 function keyPressed() {
+	console.log('Tecla pulsado');
 	if (key == ' ' && canJump) {
-		velocidad = fuerzaSalto;
-        checkCollision();
+		console.log('Spacebar Pulsado y puede saltar');
+		console.log(`Velocidad: ${pruebaFlappy.velocidad} Fuerza salto: ${pruebaFlappy.fuerzaSalto}`);
+
+		pruebaFlappy.velocidad = pruebaFlappy.fuerzaSalto;
 		canJump = false;
+		console.log(`Salto realizado: Velocidad: ${pruebaFlappy.velocidad} Fuerza salto: ${pruebaFlappy.fuerzaSalto}`);
+
 	}
-}
-
-function checkCollision() {
-
-}
-
-//clase para los flappys
-class Flappy {
-
 }
 
 // Clase que representa las tuberías
 class Tuberia {
-	constructor(x) {
-		this.x = x;
+	constructor(xStart) {
+		this.xStart = xStart;
+		this.xEnd = this.xStart + 50;
 		this.posY = height - 200;
 	}
 
 	// Mueve la tubería hacia la izquierda
 	mover() {
-		this.x -= velocidadTuberias;
+		this.xStart -= velocidadTuberias;
 	}
 
 	// Dibuja la tubería en pantalla
 	mostrar() {
-		image(tuberiaInferior, this.x, this.posY, anchoTuberia, tuberiaInferior.height);
+		image(tuberiaInferior, this.xStart, this.posY, anchoTuberia, tuberiaInferior.height);
 	}
 
 	// Verifica si la tubería ha salido de la pantalla
 	fueraPantalla() {
-		return this.x < -anchoTuberia;
+		return this.xStart < -anchoTuberia;
 	}
 
 	// Reposiciona la tubería a la derecha de la pantalla con separación constante
 	reiniciar(nuevaX) {
-		this.x = nuevaX;
+		this.xStart = nuevaX;
+	}
+}
+
+//clase para los flappys
+class Flappy {
+	constructor() {
+		this.x = 100;
+		this.y = 200;
+		this.alto = 30;
+		this.ancho = 40;
+		this.velocidad = velocidad;
+		this.gravedad = gravedad;
+		this.fuerzaSalto = fuerzaSalto;
+		this.puntuacion = 0;
+		this.estaVivo = true;
+	}
+
+	update() {
+		// Aplica la gravedad al pájaro
+		this.velocidad += this.gravedad;
+		this.y += this.velocidad;
+		this.checkCollision();
+	}
+
+	checkCollision() {
+		//console.log('Dentro de checkCollision flappy');
+	}
+
+	show() {
+		// pinta el flappy img, x, y, width, height
+		image(pajaro, this.x, this.y, this.ancho, this.alto);
 	}
 }
